99cd4fd92efe8b17a84814034dcc6eeec994cf12
apdyqafaar <qudhacle43@gmail.com>
2026-08-09 11:50:26 +0300

    fixed reports route issue

--- routes/report.ts
+++ routes/report.ts
@@ -4,9 +4,9 @@ import { protect } from '../middleware';
 import { getDailyReports, getWeeklyReports,getMonthlyReports } from '../controller/reportContoroler';
 
 const router=express.Router();
-router.post("/daily", protect, getDailyReports)
-router.post("/weekly", protect, getWeeklyReports)
-router.post("/monthly", protect, getMonthlyReports)
+router.get("/daily", protect, getDailyReports)
+router.get("/weekly", protect, getWeeklyReports)
+router.get("/monthly", protect, getMonthlyReports)
 
 export default router;
 
